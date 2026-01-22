5c4c419d1b4986737aa5bd1a8caa9b99df3afc99
omarulises1 <omarulises414@gmail.com>
2026-01-22 16:42:09 -0600

    Agrupar rutas

--- routes/web.php
+++ routes/web.php
@@ -4,40 +4,41 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController; //Importar
 use Illuminate\Support\Facades\Route;
 
-/*
+
 Route::get('/', [HomeController::class,'index']);
 
-//Ruta para motrar el listado de registros
-Route::get('posts',[PostController::class,'index'])
-    ->name('posts.index');
+/*
+Route::prefix('posts')->name('posts.')->controller(PostController::class)->group(function(){
+    
+    //Ruta para motrar el listado de registros
+    Route::get('/','index')->name('index');
 
-//Ruta para mostrar un formulario para crear un registro
-Route::get('/posts/create',[PostController::class,'create'])
-    ->name('posts.create');
-//Ruta para guardar un registro
-Route::get('/posts',[PostController::class,'store'])
-    ->name('posts.store');
+    //Ruta para mostrar un formulario para crear un registro
+    Route::get('/create','create')->name('create');
 
-//Ruta para mostrar un registro
-Route::get('/posts/{post}',[PostController::class,'show'])
-    ->name('posts.show');
+    //Ruta para guardar un registro
+    Route::get('/','store')->name('store');
 
-//Ruta para mostrar un formulario para editar un registro
-Route::get('/posts/{post}/edit',[PostController::class,'edit'])
-    ->name('posts.edit');
+    //Ruta para mostrar un registro
+    Route::get('/{post}','show')->name('show');
 
-//Ruta para actualizar un registro
-Route::put('posts{post}', [PostController::class,'update'])
-    ->name('posts.update');
+    //Ruta para mostrar un formulario para editar un registro
+    Route::get('/{post}/edit','edit')->name('edit');
 
-//Ruta para eliminar un registro
-Route::put('posts{post}', [PostController::class,'destroy'])
-    ->name('posts.destroy');
-*/
+    //Ruta para actualizar un registro
+    Route::put('/{post}','update')->name('update');
+
+    //Ruta para eliminar un registro
+    Route::put('/{post}','destroy')->name('destroy');
+}
+);
+
 
 //Cambio de URI
 Route::resource('articulos',PostController::class)
     ->parameters(['articulos'=>'post'])
     ->names('posts');
     //->only(['index']);
+*/
 
+Route::resource('posts',PostController::class);
